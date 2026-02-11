3ba86b4fa027c926bde3156dd5cdd8b722b3d3c4
Gganaa22 <darkhanbaatargantogtokh47@email.com>
2026-02-11 14:26:00 +0800

    Laborator2

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -5,11 +5,15 @@ import edu.cmu.cs.cs214.lab02.shapes.*;
 public class Main {
     public static void main(String[] args) {
         Shape shape = new Rectangle(2, 3);
-        //Shape shape = new Circle(3);
-        //Shape shape = new Square(4);
+        Shape shape1 = new Circle(3);
+        Shape shape3 = new Square(4);
 
         Renderer renderer = new Renderer(shape);
+        Renderer renderer1 = new Renderer(shape1);
+        Renderer renderer2 = new Renderer(shape3);
 
         renderer.draw();
+        renderer1.draw();
+        renderer2.draw();
     }
 }
